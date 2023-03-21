804f5ce929b79780c505a930092b1d8a57b4f483
Nyleco-J <anuran.nyleco24@gmail.com>
2023-03-21 15:40:15 +0800

    documents-full

--- dist/developer.js
+++ dist/developer.js
@@ -1,6 +1,14 @@
+// const toggleMenu1 = document.querySelector(".toggle__menu1");
+// const plat = document.querySelector(".docu ul");
+// toggleMenu1.addEventListener("click", () => {
+//   toggleMenu1.classList.toggle("open");
+//   plat.classList.toggle("open");
+// });
+
 const toggleMenu1 = document.querySelector(".toggle__menu1");
-const plat = document.querySelector(".docu ul");
+const headerNav1 = document.querySelector(".navi ul");
 toggleMenu1.addEventListener("click", () => {
   toggleMenu1.classList.toggle("open");
-  plat.classList.toggle("open");
+  headerNav1.classList.toggle("open");
+  headerNav1.style=("transition: 300ms");
 });
